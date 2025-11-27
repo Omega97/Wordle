7dff959ffdc6e2ea5fecafc76d42910dad7f4243
Omar Cusma Fait <31404553+Omega97@users.noreply.github.com>
2025-11-27 18:46:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 """ This program helps you solve today's Wordle. """
 from scripts.wordlehelper import WordleHelper
 from src.color_rules import ColorRules
+#todo TINGE not allowed solution?
 
 
-def main(n_iter=100_000, c=2.):
+def main(n_iter=300_000, c=1.5):
     """
     Good starters: CRANE, SNARE, STARE, TRACE, CRATE, ...
     """
     rules = ColorRules()
 
     # Color rules (ADD RULES HERE)
-    rules.add_rule(guess='CRANE', code = '____E')
-    # rules.add_rule(guess='LOINS', code = '_____')
+    # rules.add_rule(guess='CRATE', code='____e')
+    # rules.add_rule(guess='SOLID', code='_Ol__')
 
     # Run the computation
     wordle_helper = WordleHelper()
